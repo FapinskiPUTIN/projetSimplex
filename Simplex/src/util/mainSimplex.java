272751f814b12d3c6a243414a8ae0af9d15702f7
MrBiscotto <floriangenicq@gmail.com>
2017-11-26 17:29:25 +0100

    Avancement number one

--- Simplex/src/util/mainSimplex.java
+++ Simplex/src/util/mainSimplex.java
@@ -7,12 +7,52 @@ public class mainSimplex {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner lc = new Scanner (System.in);
-		int nbVar, nbCont;
-		int var;
+		int nbVar = 0, nbCont;
+		String valTemp;
+		String valZTemp;
+		double valCont;
+		
+		
+		
+		
 		System.out.println("Nombre de variables : ");
 		nbVar = lc.nextInt();
+		double[] valInt = new double[nbVar];
+		String valeurs[] = new String[nbVar];
+		lc.nextLine();
+		
+		System.out.println("Entrez votre Z : ");
+		valZTemp=lc.nextLine();
+		String valZ[] = new String[nbVar];
+		double[] valZD = new double[nbVar];
+		
+		valZ = valZTemp.split(" +");
+		for(int j=0;j<valZ.length;j++) {
+			valZD[j]=Double.parseDouble(valZ[j]);
+			System.out.println("val "+j+" : "+valZD[j]);
+		}
+		
 		System.out.println("Nombre de contraintes : ");
 		nbCont = lc.nextInt();
+		lc.nextLine();
+		
+		for(int i=0;i<nbCont;i++) {
+		System.out.println("Entrez les valeurs de cette manière (ex : 1 2 8 12 ...)");
+		valTemp=lc.nextLine();
+		System.out.println("Entrez la contrainte");
+		valCont=lc.nextDouble();
+		lc.nextLine();
+		
+		valeurs = valTemp.split(" +");
+
+		for(int j=0;j<valeurs.length;j++) {
+			valInt[j]=Double.parseDouble(valeurs[j]);
+			System.out.println("val "+j+" : "+valInt[j]);
+			
+		}
+			System.out.println(valCont);
+			
+		}
 		System.out.println("au revoir");
 		
 		
