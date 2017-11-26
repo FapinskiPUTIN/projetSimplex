1ca5deca89d9bbd08f3885ec00f4dd7ede242b86
FapinskiPUTIN <medericallard@gmail.com>
2017-11-26 15:21:01 +0100

    test2 - Med

--- Simplex/src/modele/Matrice.java
+++ Simplex/src/modele/Matrice.java
@@ -1,5 +1,7 @@
 package modele;
 
 public class Matrice {
-
+	public Matrice() {
+	
+	}
 }
--- test/src/test/main.java
+++ test/src/test/main.java
@@ -13,17 +13,18 @@ public class main {
 		x.add(3);
 		x.add(5);
 		y.add(x);
+		x = new ArrayList<>();
 		System.out.println(y.get(y.size()-1));
-		x.add(7);
-		x.add(8);
-		x.add(9);
+		x.add(1);
+		x.add(4);
+		x.add(5);
 		y.add(x);
 		y.set(1, x);
 		
-		System.out.println(y.indexOf(x));
+		System.out.println(y.indexOf(0));
 		System.out.println(y.size());
-		System.out.println(y.get(y.size()-1));
-		
+		System.out.println(y.get(0));
+		System.out.println(y.get(1));
 	}
 
 }
