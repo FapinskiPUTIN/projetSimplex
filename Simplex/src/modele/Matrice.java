17591d012ddfaeeb4b201d0ed8358e30a50181a4
MrBiscotto <floriangenicq@gmail.com>
2017-11-29 16:27:54 +0100

    test

--- Simplex/src/modele/Matrice.java
+++ Simplex/src/modele/Matrice.java
@@ -15,9 +15,23 @@ public class Matrice {
 		}
 		matrice.add(ligne);
 	}
+	public String equation(double tabLigne[]) {
+		String temp="";
+		for(int i=0;i<tabLigne.length;i++) {
+			if(i<tabLigne.length) {
+				temp=tabLigne[i]+"x"+(i+1)+"+";
+			}else if(i<tabLigne.length-2) {
+				temp=tabLigne[i]+"x"+(i+1)+"<=";
+			}else {
+				temp=tabLigne[i]+"";
+			}
+		}
+		return temp;
+	}
+
 	@Override
 	public String toString() {
 		return "Matrice [matrice=" + matrice + "]";
 	}
-	
+
 }
--- Simplex/src/util/mainSimplex.java
+++ Simplex/src/util/mainSimplex.java
@@ -1,8 +1,10 @@
 package util;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 import modele.Matrice;
+import sauvegarde.Serialisation;
 
 public class mainSimplex {
 
@@ -14,42 +16,70 @@ public class mainSimplex {
 		String valTemp;
 		String valZTemp;
 		double valCont;
-		
+		String temp[] ;
+
+		Serialisation s = new Serialisation();
+
 		System.out.println("Nombre de variables : ");
 		nbVar = lc.nextInt();
 		double[] valInt = new double[nbVar+1];
 		String valeurs[] = new String[nbVar+1];
 		lc.nextLine();
-		
+
 		System.out.println("Entrez votre Z : ");
 		valZTemp=lc.nextLine();
 		String valZ[] = new String[nbVar+1];
 		double[] valZD = new double[nbVar+1];
-		
+
 		valZ = valZTemp.split(" +");
+		
 		for(int j=0;j<valZ.length;j++) {
 			valZD[j]=Double.parseDouble(valZ[j]);
 			System.out.println("val "+j+" : "+valZD[j]);
 		}
-		
+
 		System.out.println("Nombre de contraintes : ");
 		nbCont = lc.nextInt();
 		lc.nextLine();
+		temp= new String[nbCont];
 		
 		for(int i=0;i<nbCont;i++) {
 			System.out.println("Entrez les valeurs de cette manière (ex : 1 2 8 12 ...)");
 			valTemp=lc.nextLine();
-			/*System.out.println("Entrez la contrainte");
-			valCont=lc.nextDouble();
-			lc.nextLine();*/
+
 			valeurs = valTemp.split(" +");
 			for(int j=0;j<valeurs.length;j++) {
 				valInt[j]=Double.parseDouble(valeurs[j]);
 				System.out.println("val "+j+" : "+valInt[j]);	
 			}
+
+			
+			temp[i]="";
+			for(int k=0;k<valInt.length;k++) {
+
+				if(k<valInt.length && k!=valInt.length-2 && k!=valInt.length-1 ) {
+					temp[i]+=valInt[k]+"x"+(k+1)+" + ";
+				}else if(k==(valInt.length-2)) {
+					temp[i]+=valInt[k]+"x"+(k+1)+" <= ";
+				}else if(k==valInt.length-1) {
+					temp[i]+=valInt[k]+"";
+				}
+			}	
 			m.creerLigne(valInt, i);
 		}
 		System.out.println(m.toString());
+
+		for(int i=0;i<temp.length;i++) {
+			System.out.println(temp[i]); //Il affichera ici Null pour l'élément 0
+			s.ecrireFichierEnnonce("test1", temp[i]);
+			s.ecrireFichierTexteEnnonce("test1", temp[i]);
+
+		}
+
+		
+		
+
+
 		System.out.println("au revoir");
 	}
 
